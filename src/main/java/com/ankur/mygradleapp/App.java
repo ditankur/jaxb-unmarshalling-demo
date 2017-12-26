84e1e87d2d107bebc426f5cdbee8645c4f7652df
LAPTOP-GHAC7N8O\ankur <ankur.or.sharma@gmail.com>
2017-12-26 21:45:58 +0000

    Code refactoring to make this sample project a little bit more useful.

--- src/main/java/com/ankur/mygradleapp/App.java
+++ src/main/java/com/ankur/mygradleapp/App.java
@@ -2,29 +2,18 @@ package com.ankur.mygradleapp;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) throws JAXBException {
-
-//        JAXBContext context = JAXBContext.newInstance(Duck.class);
-//        Marshaller marshaller= context.createMarshaller();
-//        marshaller.marshal(new Duck("donald","white","pond"),new File("duck.xml"));
-//        System.out.println("Success");
-//
-//        Unmarshaller unmarshaller=context.createUnmarshaller();
-//        Duck duck= (Duck)unmarshaller.unmarshal(new File("duck.xml"));
-//        System.out.print(duck);
-
-
         JAXBContext context = JAXBContext.newInstance(DucksType.class);
-        Unmarshaller unmarshaller=context.createUnmarshaller();
-        DucksType duckList=(DucksType) unmarshaller.unmarshal(new File("duck1.xml"));
-        List<DuckType> myducks= duckList.getDuck();
-        System.out.println(myducks);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        DucksType ducks = (DucksType) unmarshaller.unmarshal(new File("ducks.xml"));
+        List<DuckType> listOfDucks = ducks.getDuck();
+        listOfDucks.forEach(duckType ->
+                System.out.println(duckType.getName() + " " + duckType.getColor() + " " + duckType.getHome()));
     }
 }
 
